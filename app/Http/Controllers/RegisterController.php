a00635f52fc1c931d3783311d915da165df650fa
LeeDiay <leducanh1503.works@gmail.com>
2024-04-05 13:30:23 +0700

    update

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -15,8 +15,8 @@ class RegisterController extends Controller
     public function store(){
 
         $attributes = request()->validate([
-            'username' => 'required|max:20|min:6|regex:/^\S*$/',
-            'name' => 'required|max:30',
+            'username' => 'required|max:20|min:6|regex:/^[a-zA-Z0-9_]+$/',
+            'name' => 'required|max:30|regex:/^[a-zA-Z\sàáảãạăắằẳẵặâấầẩẫậèéẻẽẹêếềểễệđìíỉĩịòóỏõọôốồổỗộơớờởỡợùúủũụưứừửữựỳỹỷỵÀÁẢÃẠĂẮẰẲẴẶÂẤẦẨẪẬÈÉẺẼẸÊẾỀỂỄỆĐÌÍỈĨỊÒÓỎÕỌÔỐỒỔỖỘƠỚỜỞỠỢÙÚỦŨỤƯỨỪỬỮỰỲỸỶỴ\s]+$/u',
             'email' => 'required|email|max:255|unique:users,email',
             'phone' => 'required|max:15',
             'password' => 'required|min:8|max:255',
